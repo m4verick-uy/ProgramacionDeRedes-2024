9db5dfcef44553a49c08b2a917a1e71096f04a8a
m4verick-uy <gechichure@gmail.com>
2024-08-21 16:13:21 -0300

    update - Agrego backgroundThreadsExample2.csproj

---
+++ backgroundThreadsExample2/PriorityTest.cs
@@ -0,0 +1,13 @@
+﻿namespace backgroundThreadsExampl;
+
+using System.Threading;
+
+class PriorityTest
+{
+    static void Main (string[] args)
+    {
+        Thread worker = new Thread ( () => Console.ReadLine() );
+        if (args.Length > 0) worker.IsBackground = true;
+        worker.Start();
+    }
+}
