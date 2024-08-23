bb0fa0d99c4fc3872480f8b5d79feacad70c4af1
m4verick-uy <gechichure@gmail.com>
2024-08-23 14:52:33 -0300

    Ejercicio 1 resuelto

---
+++ Practico_1/Ejercicio1/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Ejercicio1;
+
+using System.Threading;
+
+
+// Solución usando Join()
+
+/*
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Creo los hilos
+        Thread t1 = new Thread(PrintX);
+        Thread t2 = new Thread(PrintY);
+        
+        //Lanzo los hilos
+        t1.Start();
+        t1.Join();
+        
+        t2.Start();
+        t2.Join();
+    }
+
+    static void PrintX()
+    {
+        for (int i = 1; i <=20; i++)
+        {
+            Console.Write('X');
+            Thread.Sleep(100);
+        }
+    }
+
+    static void PrintY()
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.Write('Y');
+            Thread.Sleep(100);
+        }
+    }
+}
+*/
+
+// Solucion usando AutoResetEvent para sincronizar los threads
+
+class Program
+{
+    static AutoResetEvent autoEvent = new AutoResetEvent(false);
+    
+    static void Main(string[] args)
+    {
+        //Creo los hilos
+        Thread t1 = new Thread(PrintX);
+        Thread t2 = new Thread(PrintY);
+        
+        //Lanzo los hilos
+        t1.Start();
+        t2.Start();
+    }
+
+    static void PrintX()
+    {
+        for (int i = 1; i <=20; i++)
+        {
+            Console.Write('X');
+            Thread.Sleep(100);
+        }
+        autoEvent.Set(); //Notifico que PrintX terminó
+    }
+
+    static void PrintY()
+    {
+        autoEvent.WaitOne(); //Espero hasta que PrintX termine
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.Write('Y');
+            Thread.Sleep(100);
+        }
+    }
+}
---
+++ Practico_1/Ejercicio1/README.md
@@ -0,0 +1,14 @@
+# Práctico 1
+
+### Ejercicio 1)
+
+Desarrolle un programa que tenga 2 hilos de ejecución, uno que imprima 20 ‘x’, y el otro que
+imprima 20 ‘y’, entre la impresión de cada carácter haga una espera de 100 mseg. Logre que
+primero se impriman todas las ‘x’. Haga esto de al menos 2 formas distintas.
+
+### Soluciones
+
+- **Primera Forma:** Uso de `Thread` y `Join`.
+- **Segunda Forma:** Uso de `Thread` y `AutoResetEvent` para sincronización.
+
+#### Autor: [**Guillermo Echichure**](mailto:GE151381@fi365.ort.edu.uy)
