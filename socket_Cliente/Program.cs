40ab39e427ae859adb202feec205065fbc5376d8
Guillermo Echichure <gechichure@gmail.com>
2024-09-11 17:29:35 -0300

    update

---
+++ socket_Cliente/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace socket_Cliente;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ socket_Servidor/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace socket_Servidor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
