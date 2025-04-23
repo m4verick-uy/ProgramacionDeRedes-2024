ac2e17e211ec1eecc925002118ad5741335c9fc3
Guillermo Echichure <gechichure@gmail.com>
2025-04-23 20:23:58 -0300

    Se agregan Dockerfiles y docker-compose

---
+++ Client/Client.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Cliente
+{
+    public class Client
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando app Cliente...!");
+
+            var socketCliente = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 0); // Puerto 0 toma el primero disponible
+            socketCliente.Bind(localEndPoint);
+
+            // El endpoint del servidor
+            var remoteEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 10000);
+
+            socketCliente.Connect(remoteEndPoint); // Nos conectamos al servidor
+
+            Console.WriteLine("Me conecté al servidor!!!! ");
+
+            Console.WriteLine("Escriba un mensaje y presione enter");
+            while (true)
+            {
+                string mensaje = Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(mensaje);
+                socketCliente.Send(data);
+            }
+
+            Console.WriteLine("Cerrando la conexion");
+
+            socketCliente.Shutdown(SocketShutdown.Both);
+            socketCliente.Close();
+
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Servidor
+{
+    public class Server
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando app Servidor.....");
+
+            //creo el socket
+            var socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 10000);
+            // creo el endpoint con IP y puerto  (Estos son conocidos)
+
+            socketServer.Bind(localEndPoint);  // Vinculamos el Socket y el endpoint
+
+            socketServer.Listen(10);  // Ponemos el socket en modo escucha
+            bool salir = false;
+            while (!salir)
+            {
+
+                var socketClient = socketServer.Accept(); // BLOQUEANTE
+                Console.WriteLine("Acepte un cliente");
+
+                new Thread(() => HandleClient(socketClient)).Start();
+                // Lanzo un hilo por cada cliente
+
+
+
+            }
+
+            //Console.ReadLine();
+
+        }
+
+        static void HandleClient(Socket socketCliente)
+        {
+            Console.WriteLine("Atendiendo a un cliente");
+            bool isConected = true;
+            while (isConected)
+            {
+                byte[] data = new byte[1024];
+                socketCliente.Receive(data);
+                string mensaje = Encoding.UTF8.GetString(data);
+                Console.WriteLine(mensaje);
+
+            }
+            Console.WriteLine("Cliente desconectado");
+
+        }
+    }
+}
