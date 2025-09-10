cf82c118966e8c08447cd468ec580bb6ec6eafcb
Guillermo Echichure <gechichure@gmail.com>
2025-09-10 16:48:45 -0300

    Primer ejemplo con Cliente y Servidor realizando la primera comunicacion.

---
+++ Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear el socket
+            var socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // creamos un endpont, una asociacion IP/puerto
+
+            // endpoint cliente local 
+            var localEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 15000);
+
+            var remoteEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 20000);
+
+            // vincular el endpoint con el socket 
+            socketClient.Bind(localEndpoint);
+
+            // cliente se conecta al servidor
+            socketClient.Connect(remoteEndpoint);
+
+
+
+            Console.WriteLine("Connected to server!!!!");
+            Console.ReadLine();
+            Console.WriteLine("Will Close Connection...");
+            socketClient.Shutdown(SocketShutdown.Both); 
+            socketClient.Close(); 
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting server - Waiting for clients connections ......");
+            
+            // Creo la clase Sockets
+            var socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var localEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 20000); // De 0  a 65535 pero hay algunos ptos reservados, mejor > 20000
+
+            //Vinculamos el socket con el endpoint
+            socketServer.Bind(localEndpoint);
+
+            socketServer.Listen(1); 
+
+            var socketClient = socketServer.Accept(); // OJO CON ESTA OPEARACION ES BLOQUEANTE !!!!
+
+
+            Console.WriteLine("Solo se imprime una vez que el servidor acepto una conexion"); 
+            Console.ReadLine();
+        }
+    }
+}
