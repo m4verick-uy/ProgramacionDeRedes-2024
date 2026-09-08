826af2952e8b6d977c412e7c609dbcc2bce74ff0
m4verick-uy <gechichure@gmail.com>
2026-09-08 12:32:59 -0300

    Primer Ejemplo cliente-servidor

---
+++ EjemploSockets/Client/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Client;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Iniciando aplicación cliente !!");
+        
+        // creamos el socket cliente
+        var socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+        // creamos el endpoint local
+        var localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 0);
+        
+        // vinculamos socket con su endpoint local
+        socketClient.Bind(localEndPoint);
+        
+        // creo el endpoint remoto del servidor 
+        var remoteEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 20000);
+        
+        // conectamos el cliente al servidor
+        socketClient.Connect(remoteEndPoint);
+        
+        Console.WriteLine("Conectado al servidor !!!");
+        
+        Console.WriteLine("Escriba un mensaje para enviar: ");
+
+        while (true)
+        {
+            string mensaje = Console.ReadLine();
+            byte[] data = Encoding.UTF8.GetBytes(mensaje); // toma un mensaje de string y lo pasa a bytes
+            socketClient.Send(data);
+        }
+        
+        // Configuro la desconexión segura
+        socketClient.Shutdown(SocketShutdown.Both);
+        socketClient.Close();
+        
+    }
+}
---
+++ EjemploSockets/Server/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Server;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+class Program
+{
+
+    static void HandleClient(Socket socketClient)
+    {
+        bool ClientConnected = true;
+
+        while (ClientConnected)
+        {
+            byte[] data = new byte[256];
+            socketClient.Receive(data);
+            string message = Encoding.UTF8.GetString(data);
+            Console.WriteLine("El cliente dice: " + message);
+        }
+        Console.WriteLine("Cliente desconectado");
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Iniciando Servidor ....");
+        
+        // crear el socket 
+        var socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+        // creamos el endpoint 
+        var localEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 20000);
+        
+        // vincular endpoint con el socket
+        
+        socketServer.Bind(localEndpoint);
+        
+        // configuramos el servidor en modo escucha
+        socketServer.Listen(3);
+        
+        // configuramos que el servidor acepte conexiones
+
+
+
+        while (true)
+        {
+            var socketClient = socketServer.Accept();  // Esta operacion es Bloqueante !!!!! CUIDADO
+            Console.WriteLine("Acepte un pedido de conexiòn");
+            
+            new Thread(()=> HandleClient(socketClient)).Start();
+        }
+        
+        
+        
+        Console.ReadLine();
+        
+        socketServer.Shutdown(SocketShutdown.Both);
+        socketServer.Close();
+    }
+}
